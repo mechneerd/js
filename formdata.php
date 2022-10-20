50641b4534157b340aee351d7d710ce9bb3c79f3
prasanth <106507229+mechneerd@users.noreply.github.com>
2022-10-20 17:39:21 +0530

    Add files via upload

---
+++ formdata.php
@@ -0,0 +1,26 @@
+﻿<html>
+<body>
+
+welcome <?php echo $_POST["username"] ;?> <br>
+
+Your password is  : <?php echo $_POST["password"] ;?><br>
+
+your fav website is : <?php echo $_POST['website']; ?><br>
+
+
+your employees id is : <?php echo $_POST['employ'];?><br>
+
+you gender is :<?php echo $_POST['gender'];?><br>
+
+your fav movies are:<br>
+<?php 
+$arr = ($_POST['movies']);
+for($i=0;$i<(count($arr));$i++){
+	echo $arr[$i].'<br>';
+}
+
+?>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <title>FORM FOR PRATICE</title>
+    
+  </head>
+
+  <body>
+	<h1 style="color:#A9A9A9; margin-left:300px; margin-top:50px;'">Fill the form </h1>
+  <div id="cont" style="background-color:#00cc99
+;display:flex;width:250px;height:300px;margin-left:300px;margin-top:20px;">
+
+	<form action="formdata.php"; method="POST" >
+		<lable>username</label>
+		<input type="Text"  name='username'></input><br>
+		<lable>password</label>
+		<input type="password" name="password"></input><br>
+	
+
+	
+	<lable>Employee number</label>
+	<input type="number" name='employ' min="1" max="99999"></input><br>
+
+	<lable>which website you want to log in to? </label>
+	<select name="website" style="background-color:#6699ff;color:#ff9900" multiple>
+		<option value="google">google</option>
+		<option value="facebook">facebook</option>
+		<option value="twitter">twitter</option>
+		<option value="quora">quora</option>
+	</select><br>
+
+	<p>Gender:</p>
+		<input type="radio" name = "gender" class="gender" value="Male" name="g" style="margin-left:105px;"></input>
+		<label>Male</label><br>
+		<input type="radio" name = "gender" class="gender" value="Female" name="g" style="margin-left:105px;"></input>
+		<label>Female</label><br>
+	<lable>MovieWatched:</label>
+		<input type="checkbox" name='movies[]'  value="BB">BB</input><br>
+		<input type="checkbox" name='movies[]'  value="RRR" style="margin-left:105px;">RRR</input><br>
+		<input type="checkbox"  name='movies[]'  value="KGF" style="margin-left:105px;">KGF</input><br>
+	<input type="submit" style="background-color:#00cc00
+;" ></input>
+	<form>
+  </div>
+  </body>
+</html>
+
